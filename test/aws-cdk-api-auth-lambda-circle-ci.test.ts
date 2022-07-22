d8092846cc625724b4898db3398a35bacae38d1d
yemiwebby <yemiwebby@gmail.com>
2022-07-22 14:02:41 +0100

    Initial commit

---
+++ test/aws-cdk-api-auth-lambda-circle-ci.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkApiAuthLambdaCircleCi from '../lib/aws-cdk-api-auth-lambda-circle-ci-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-api-auth-lambda-circle-ci-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkApiAuthLambdaCircleCi.AwsCdkApiAuthLambdaCircleCiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
